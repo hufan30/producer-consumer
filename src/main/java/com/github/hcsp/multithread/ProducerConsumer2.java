781edbe881f39e84a7f0a4eae188a005b8f8c909
xutianzi333 <37890774+xutianzi333@users.noreply.github.com>
2019-11-28 14:25:59 +0800

    5种方式实现生产者消费者模型 (#70)
    5种方式实现生产者消费者模型 (#70)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,85 @@
 package com.github.hcsp.multithread;
 
-public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-    public static class Producer extends Thread {
-        @Override
-        public void run() {}
+/**
+ * 第二种实现： Lock/Condition
+ */
+public class ProducerConsumer2 {
+    private static int MAX_QUEUE_SIZE = 100;
+    private static Lock lock = new ReentrantLock(true); // 公平可重用锁
+    private static Condition queueEmpty = lock.newCondition();
+    private static Condition queueFull = lock.newCondition();
+
+    public static void main(String[] args) throws InterruptedException {
+        Queue<Integer> queue = new LinkedList<>();
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
     }
 
-    public static class Consumer extends Thread {
+    public static class Producer extends Thread {
+        Queue<Integer> queue;
+
+        public Producer(Queue<Integer> queue) {
+            this.queue = queue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            lock.lock();
+            try {
+                for (int i = 0; i < 10; i++) {
+                    while (queue.size() >= 100) {
+                        queueEmpty.await();
+                    }
+                    int value = new Random().nextInt();
+                    System.out.println("Producing " + value);
+                    queue.add(value);
+                    queueFull.signalAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+
+    public static class Consumer extends Thread {
+        Queue<Integer> queue;
+
+        public Consumer(Queue<Integer> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            lock.lock();
+            try {
+                for (int i = 0; i < 10; i++) {
+                    while (queue.size() == 0) {
+                        queueFull.await();
+                    }
+                    System.out.println("Consumint " + queue.remove());
+                    queueEmpty.signalAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,74 @@
 package com.github.hcsp.multithread;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 第三种实现：Semaphore信号量
+ */
 public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    private static Semaphore emptySlot = new Semaphore(1);
+    private static Semaphore fullSlot = new Semaphore(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        Queue<Integer> queue = new LinkedList<>();
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        Queue<Integer> queue;
+
+        public Producer(Queue<Integer> queue) {
+            this.queue = queue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    emptySlot.acquire();
+                    synchronized (ProducerConsumer3.class) {
+                        int value = new Random().nextInt();
+                        System.out.println("Producing " + value);
+                        queue.add(value);
+                    }
+                    fullSlot.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Queue<Integer> queue;
+
+        public Consumer(Queue<Integer> queue) {
+            this.queue = queue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    fullSlot.acquire();
+                    synchronized (ProducerConsumer3.class) {
+                        System.out.println("Producing " + queue.remove());
+                    }
+                    emptySlot.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
