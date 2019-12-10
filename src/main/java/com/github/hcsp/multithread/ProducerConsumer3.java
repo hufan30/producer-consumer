3b561f997ad30de33b90929343ab12aba969b9c2
baixc <969552017@qq.com>
2019-12-10 21:32:43 +0800

    实现一个生产者消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,57 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+        public static void main(String[] args) throws InterruptedException {
+            BlockingQueue<Integer> blockingQueue = new LinkedBlockingDeque<>();
+            Producer producer = new Producer(blockingQueue);
+            Consumer consumer = new Consumer(blockingQueue);
+
+            producer.start();
+            consumer.start();
+
+            producer.join();
+            producer.join();
+        }
 
     public static class Producer extends Thread {
+        private BlockingQueue<Integer> blockingQueue;
+        Producer (BlockingQueue<Integer> blockingQueue) {
+            this.blockingQueue = blockingQueue;
+        }
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                Integer randomNum = new Random().nextInt();
+                try {
+                    blockingQueue.put(randomNum);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Producing " + randomNum);
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private BlockingQueue<Integer> blockingQueue;
+        Consumer(BlockingQueue<Integer> blockingQueue) {
+            this.blockingQueue = blockingQueue;
+        }
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                int headNum = 0;
+                try {
+                    headNum = blockingQueue.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Consuming " + headNum);
+            }
+        }
     }
 }
