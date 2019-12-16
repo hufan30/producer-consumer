a7f4609a610ab27be0803ea594c1d41a7eca2a11
恶魔妹妹买面膜 <51756846+chelegen@users.noreply.github.com>
2019-12-16 23:13:11 +0800

    synchronized (#72)
    synchronized (#72)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,7 +1,15 @@
 package com.github.hcsp.multithread;
 
+import java.awt.*;
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
+    private static Optional<Integer> value;
+
     public static void main(String[] args) throws InterruptedException {
+        value = Optional.empty();
+
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
 
@@ -14,11 +22,42 @@ public class ProducerConsumer1 {
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized ("lock") {
+                    while (value.isPresent()) {
+                        try {
+                            "lock".wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    int random = new Random().nextInt();
+                    System.out.println("Producing " + random);
+                    value = Optional.of(random);
+                    "lock".notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized ("lock") {
+                    while (!value.isPresent()) {
+                        try {
+                            "lock".wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + value.get());
+                    value = Optional.empty();
+                    "lock".notify();
+                }
+            }
+        }
     }
 }
