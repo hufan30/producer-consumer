352325e9a5a9dadd82d2471257a3421c90732b54
87244099 <872440899@qq.com>
2019-12-29 10:12:47 +0800

    用5种方式实现生产者消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,14 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Container container = new Container();
+        Object lock = new Object();
+        Producer producer = new Producer(lock, container);
+        Consumer consumer = new Consumer(lock, container);
 
         producer.start();
         consumer.start();
@@ -12,13 +17,72 @@ public class ProducerConsumer1 {
         producer.join();
     }
 
+    public static class Container{
+        private Optional<Integer> value = Optional.empty();
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
+    }
+
     public static class Producer extends Thread {
+        private Container container;
+        private Object lock;
+        public Producer(Object lock, Container container){
+            this.lock = lock;
+            this.container = container;
+        }
         @Override
-        public void run() {}
+        public void run() {
+            for (int i=0; i<10; i++){
+                synchronized (lock){
+                    if (container.getValue().isPresent()){
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                    int value = new Random().nextInt();
+                    System.out.println("Producing "+value);
+                    container.setValue(Optional.of(value));
+                    lock.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+        Object lock;
+        public Consumer(Object lock, Container container){
+            this.lock = lock;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i=0; i<10; i++){
+                synchronized (lock){
+                    if (!container.getValue().isPresent()){
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                    int value = container.getValue().get();
+                    System.out.println("Consuming "+value);
+                    container.setValue(Optional.empty());
+                    lock.notify();
+                }
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,70 @@
 package com.github.hcsp.multithread;
 
-public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.ReentrantLock;
 
-    public static class Producer extends Thread {
-        @Override
-        public void run() {}
+public class ProducerConsumer3 {
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        BlockingQueue<Integer> blockingQueue = new LinkedBlockingQueue(1);
+        BlockingQueue<Integer> signalQueue = new LinkedBlockingQueue(1);
+        Producer producer = new Producer(blockingQueue, signalQueue);
+        Consumer consumer = new Consumer(blockingQueue, signalQueue);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
     }
 
-    public static class Consumer extends Thread {
+    public static ReentrantLock lock = new ReentrantLock();
+
+    public static class Producer extends Thread {
+        BlockingQueue<Integer> queue;
+        BlockingQueue<Integer> signalQueue;
+        public Producer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signalQueue){
+            this.queue = queue;
+            this.signalQueue = signalQueue;
+        }
         @Override
-        public void run() {}
+        public void run() {
+            for (int i=0; i<10; i++){
+                try {
+                    int value = new Random().nextInt();
+                    queue.put( value );
+                    System.out.println("Producing "+value);
+                    signalQueue.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+    public static class Consumer extends Thread {
+        BlockingQueue<Integer> queue;
+        BlockingQueue<Integer> signalQueue;
+        public Consumer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signalQueue){
+            this.queue = queue;
+            this.signalQueue = signalQueue;
+        }
+
+        @Override
+        public void run() {
+            for (int i=0; i<10; i++){
+                try {
+                    System.out.println("Consuming "+queue.take());
+                    signalQueue.put(-1);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
