4575b5035ee8f67d53447e4165a2880b73f2cebe
Maricaya <43136257+Maricaya@users.noreply.github.com>
2020-01-03 09:29:15 +0800

    3种方式实现生产者和消费者 (#80)
    3种方式实现生产者和消费者 (#80)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,15 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Container container = new Container();
+        Object lock = new Object();
+
+        Producer producer = new Producer(container, lock);
+        Consumer consumer = new Consumer(container, lock);
 
         producer.start();
         consumer.start();
@@ -13,12 +19,72 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        Container container;
+        Object lock;
+
+        public Producer(Container container, Object lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (container.getContainer().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    int r = new Random().nextInt();
+                    container.setContainer(Optional.of(r));
+                    System.out.println("Producing " + r);
+                    lock.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+        Object lock;
+
+        public Consumer(Container container, Object lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (!container.getContainer().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    System.out.println("Consuming " + container.getContainer().get());
+                    container.setContainer(Optional.empty());
+                    lock.notify();
+                }
+            }
+        }
+    }
+
+    static class Container {
+        private Optional<Integer> container = Optional.empty();
+
+        public Optional<Integer> getContainer() {
+            return container;
+        }
+
+        public void setContainer(Optional<Integer> container) {
+            this.container = container;
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,69 @@
 package com.github.hcsp.multithread;
 
-public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-    public static class Producer extends Thread {
-        @Override
-        public void run() {}
+public class ProducerConsumer3 {
+    public static void main(String[] args) throws InterruptedException {
+        LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>(1);
+        LinkedBlockingQueue<Integer> signalQueue = new LinkedBlockingQueue<>(1);
+
+
+        Producer producer = new Producer(queue, signalQueue);
+        Consumer consumer = new Consumer(queue, signalQueue);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
     }
 
-    public static class Consumer extends Thread {
+    public static class Producer extends Thread {
+        BlockingQueue<Integer> queue;
+        BlockingQueue<Integer> signalQueue;
+
+        public Producer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signalQueue) {
+            this.queue = queue;
+            this.signalQueue = signalQueue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                int r = new Random().nextInt();
+                System.out.println("Producing " + r);
+                try {
+                    queue.put(r);
+                    signalQueue.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    static class Consumer extends Thread {
+        BlockingQueue queue;
+        BlockingQueue signalQueue;
+
+        Consumer(BlockingQueue queue, BlockingQueue signalQueue) {
+            this.queue = queue;
+            this.signalQueue = signalQueue;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    System.out.println("Consuming " + queue.take());
+                    signalQueue.put(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
