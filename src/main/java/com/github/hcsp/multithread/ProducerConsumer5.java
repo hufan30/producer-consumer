7d77a006c24cc4de55947547c4421004b31fc7f8
ws1603 <48832455+ws1603@users.noreply.github.com>
2020-01-08 14:03:10 +0800

    producer-consumer (#81)
    producer-consumer (#81)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
@@ -1,24 +1,59 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+
 public class ProducerConsumer5 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        Exchanger<Integer> exchanger = new Exchanger<>();
+        Producer producer = new Producer(exchanger);
+        Consumer consumer = new Consumer(exchanger);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        Exchanger<Integer> exchanger;
+
+        Producer(Exchanger<Integer> exchanger) {
+            this.exchanger = exchanger;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                int r = new Random().nextInt();
+                System.out.println("Producing " + r);
+                try {
+                    exchanger.exchange(r);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Exchanger<Integer> exchanger;
+
+        Consumer(Exchanger<Integer> exchanger) {
+            this.exchanger = exchanger;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    int value = exchanger.exchange(null);
+                    System.out.println("Consuming " + value);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
