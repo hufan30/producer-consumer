f2cf1c508bd2a37d69e16873f790d12b8b8c3c9c
ws1603 <18388866683@163.com>
2020-01-08 14:01:34 +0800

    producer-consumer

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,67 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        BlockingQueue<Integer> queue = new LinkedBlockingQueue<>(1);
+        BlockingQueue<Integer> signalQueue = new LinkedBlockingQueue<>(1);
+
+        Producer producer = new Producer(queue, signalQueue);
+        Consumer consumer = new Consumer(queue, signalQueue);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        BlockingQueue<Integer> queue;
+        BlockingQueue<Integer> signalQueue;
+
+        Producer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signalQueue) {
+            this.queue = queue;
+            this.signalQueue = signalQueue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                int r = new Random().nextInt();
+                System.out.println("Producing " + r);
+                try {
+                    queue.put(r);
+                    signalQueue.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        BlockingQueue<Integer> queue;
+        BlockingQueue<Integer> signalQueue;
+
+        Consumer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signalQueue) {
+            this.queue = queue;
+            this.signalQueue = signalQueue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    System.out.println("Consuming " + queue.take());
+                    signalQueue.put(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
