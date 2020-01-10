c04fedd968a180baa8c2a710bf03ec5098918f44
丁胜 <dingduoduo0916@gmail.com>
2020-01-11 00:11:38 +0800

    Dsying4 (#86)
    Dsying4 (#86)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,12 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Container container = new Container();
+        Producer producer = new Producer(container);
+        Consumer consumer = new Consumer(container);
 
         producer.start();
         consumer.start();
@@ -13,12 +16,62 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        Container container;
+
+        public Producer(Container container) {
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (container) {
+                    while (container.value != null) {
+                        try {
+                            container.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    int random = new Random().nextInt();
+                    System.out.println("Producing " + random);
+                    container.value = random;
+
+                    container.notify();
+                }
+            }
+
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+
+        public Consumer(Container container) {
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (container) {
+                    while (container.value == null) {
+                        try {
+                            container.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + container.value);
+                    container.value = null;
+                    container.notify();
+                }
+            }
+
+        }
+    }
+
+    static class Container {
+        Object value;
     }
 }
