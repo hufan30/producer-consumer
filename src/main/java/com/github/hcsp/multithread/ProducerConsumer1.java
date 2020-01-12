62785ca2e374f0a5941c9d2c76a3a574f686ed67
zaker-666 <1075151259china@gmail.com>
2020-01-12 15:48:36 +0800

    再次提交

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,14 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Object lock = new Object();
+        Container container = new Container();
+        Producer producer = new Producer(container, lock);
+        Consumer consumer = new Consumer(container, lock);
 
         producer.start();
         consumer.start();
@@ -13,12 +18,60 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        Container container;
+        Object obj;
+
+        public Producer(Container container, Object obj) {
+            this.container = container;
+            this.obj = obj;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (obj) {
+                    while (container.getProduct().isPresent()) {
+                        try {
+                            obj.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    int r = new Random().nextInt();
+                    System.out.println("Producing " + r);
+                    container.setProduct(Optional.of(r));
+                    obj.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+        Object obj;
+
+        public Consumer(Container container, Object obj) {
+            this.container = container;
+            this.obj = obj;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (obj) {
+                    while (!container.getProduct().isPresent()) {
+                        try {
+                            obj.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Integer integer = container.getProduct().get();
+                    System.out.println("Consuming " + integer);
+                    container.setProduct(Optional.empty());
+                    obj.notify();
+                }
+            }
+        }
     }
 }
