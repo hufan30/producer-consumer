048cd213e140424bedc1cb7f7d8b25df304cc741
JayChenFE <jaychenfe@foxmail.com>
2020-01-12 16:06:23 +0800

    实现生产者消费者模式 (#88)
    实现生产者消费者模式 (#88)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,91 @@
 package com.github.hcsp.multithread;
 
-public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-    public static class Producer extends Thread {
-        @Override
-        public void run() {}
+// Lock/Condition
+public class ProducerConsumer2 {
+    public static void main(String[] args) throws InterruptedException {
+        List<Integer> list = new ArrayList<>();
+        Lock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+
+        Producer producer = new Producer(list, lock, condition);
+        Consumer consumer = new Consumer(list, lock, condition);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
     }
 
-    public static class Consumer extends Thread {
+
+    private static class Producer extends Thread {
+        final List<Integer> list;
+        final Lock lock;
+        final Condition condition;
+
+        Producer(List<Integer> list, Lock lock, Condition condition) {
+            this.list = list;
+            this.lock = lock;
+            this.condition = condition;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+
+                lock.lock();
+                try {
+                    while (!list.isEmpty()) {
+                        condition.await();
+                    }
+                    int randomValue = new Random().nextInt();
+                    System.out.println("Producing " + randomValue);
+                    list.add(randomValue);
+                    condition.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    private static class Consumer extends Thread {
+        final List<Integer> list;
+        final Lock lock;
+        final Condition condition;
+
+        Consumer(List<Integer> list, Lock lock, Condition condition) {
+            this.list = list;
+            this.lock = lock;
+            this.condition = condition;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                lock.lock();
+                try {
+                    while (list.isEmpty()) {
+                        condition.await();
+                    }
+                    System.out.println("Consuming " + list.get(0));
+                    list.remove(0);
+                    condition.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 }
