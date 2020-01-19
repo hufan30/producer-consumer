f50fb5cc1d3fe54a093e11accc4bbe896e73e0f8
xdc <1434412784@qq.com>
2020-01-19 12:44:04 +0800

    参照视频，完成一个生产者-消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,16 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
+    //使用wait/notify 方法完成
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Container container = new Container();
+        Object lock = new Object();
+
+        Producer producer = new Producer(container, lock);
+        Consumer consumer = new Consumer(container, lock);
 
         producer.start();
         consumer.start();
@@ -12,13 +19,80 @@ public class ProducerConsumer1 {
         producer.join();
     }
 
-    public static class Producer extends Thread {
-        @Override
-        public void run() {}
+    //中间容器类
+    public static class Container {
+        private Optional<Integer> value = Optional.empty();
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
     }
 
-    public static class Consumer extends Thread {
+    //生产者
+    public static class Producer extends Thread {
+        Container container;
+        Object lock;
+
+        public Producer(Container container, Object lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (container.getValue().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    int r = new Random().nextInt();
+                    System.out.println("Producing " + r);
+                    container.setValue(Optional.of(r));
+
+                    lock.notify();
+                }
+            }
+        }
+    }
+
+    //消费者
+    public static class Consumer extends Thread {
+        Container container;
+        Object lock;
+
+        public Consumer(Container container, Object lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (!container.getValue().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    Integer value = container.getValue().get();
+                    System.out.println("Consuming " + value);
+                    container.setValue(Optional.empty());
+
+                    lock.notify();
+                }
+            }
+        }
     }
 }
