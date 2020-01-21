0074b653c87126af17abf3928e890330a8960fef
Fang Lu <huzhengen@gmail.com>
2020-01-21 15:00:05 +0800

    实现一个生产者-消费者模型 (#92)
    实现一个生产者-消费者模型 (#92)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,114 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        Container container = new Container(lock);
+
+        Producer producer = new Producer(container, lock);
+        Consumer consumer = new Consumer(container, lock);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        Container container;
+        ReentrantLock lock;
+
+        public Producer(Container container, ReentrantLock lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (container.getContainer().isPresent()) {
+                        try {
+                            container.getNotConsumedYet().await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            throw new RuntimeException();
+                        }
+                    }
+                    int r = new Random().nextInt();
+                    container.setContainer(Optional.of(r));
+                    container.getNotProducedYet().signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+        ReentrantLock lock;
+
+        public Consumer(Container container, ReentrantLock lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (!container.getContainer().isPresent()) {
+                        try {
+                            container.getNotProducedYet().await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            throw new RuntimeException();
+                        }
+                    }
+                    Integer value = container.getContainer().get();
+                    container.setContainer(Optional.empty());
+                    container.getNotConsumedYet().signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    public static class Container {
+        private Condition notConsumedYet;
+        private Condition notProducedYet;
+
+        public Container(ReentrantLock lock) {
+            this.notConsumedYet = lock.newCondition();
+            this.notProducedYet = lock.newCondition();
+        }
+
+        public Condition getNotConsumedYet() {
+            return notConsumedYet;
+        }
+
+        public Condition getNotProducedYet() {
+            return notProducedYet;
+        }
+
+        private Optional<Integer> container = Optional.empty();
+
+        public Optional<Integer> getContainer() {
+            return container;
+        }
+
+        public void setContainer(Optional<Integer> container) {
+            this.container = container;
+        }
     }
 }
