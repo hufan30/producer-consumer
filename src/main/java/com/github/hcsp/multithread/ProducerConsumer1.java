ba0b2f750343ffb9a57fc540692506a2cd77ba05
BingQin <qinbing08@gmail.com>
2020-01-29 14:28:20 +0800

    使用BIockingQueueue做练习题

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,16 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        BlockingQueue<Integer> queue = new LinkedBlockingDeque<>(1);
+        BlockingQueue<Integer> signalQueue = new LinkedBlockingDeque<>(1);
+
+        Producer producer = new Producer(queue, signalQueue);
+        Consumer consumer = new Consumer(queue, signalQueue);
 
         producer.start();
         consumer.start();
@@ -13,12 +20,48 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        BlockingQueue<Integer> queue;
+        BlockingQueue<Integer> signalQueue;
+
+        public Producer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signalQueue) {
+            this.queue = queue;
+            this.signalQueue = signalQueue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                int r = new Random().nextInt();
+                System.out.println("Producing " + r);
+                try {
+                    queue.put(r);
+                    signalQueue.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        BlockingQueue<Integer> queue;
+        BlockingQueue<Integer> signalQueue;
+
+        public Consumer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signalQueue) {
+            this.queue = queue;
+            this.signalQueue = signalQueue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    System.out.println("Consuming " + queue.take());
+                    signalQueue.put(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
