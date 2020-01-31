f2caa08a42cf4b7271d5dd5e581218bd002df0a2
spc <1124516293@qq.com>
2020-01-31 21:15:43 +0800

    多种方式实现生产者-消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,14 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+
 public class ProducerConsumer1 {
+    private static Integer random;
+
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Object lock = new Object();
+        Producer producer = new Producer(lock);
+        Consumer consumer = new Consumer(lock);
 
         producer.start();
         consumer.start();
@@ -13,12 +18,54 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        Object lock;
+
+        public Producer(Object lock) {
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (random != null) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    random = new Random().nextInt();
+                    System.out.println("Producing " + random);
+                    lock.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Object lock;
+
+        public Consumer(Object lock) {
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (random == null) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + random);
+                    random = null;
+                    lock.notify();
+                }
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,87 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    private static Integer random;
+
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+        Producer producer = new Producer(lock, condition);
+        Consumer consumer = new Consumer(lock, condition);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        ReentrantLock lock;
+        Condition condition;
+
+        public Producer(ReentrantLock lock, Condition condition) {
+            this.lock = lock;
+            this.condition = condition;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (random != null) {
+                        try {
+                            condition.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    random = new Random().nextInt();
+                    System.out.println("Producing " + random);
+                    condition.signal();
+
+                } finally {
+                    lock.unlock();
+                }
+            }
+
+        }
     }
 
     public static class Consumer extends Thread {
+        ReentrantLock lock;
+        Condition condition;
+
+        public Consumer(ReentrantLock lock, Condition condition) {
+            this.lock = lock;
+            this.condition = condition;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (random == null) {
+                        try {
+                            condition.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + random);
+                    random = null;
+                    condition.signal();
+
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 }
