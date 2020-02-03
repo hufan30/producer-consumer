77c55dbec632c1e8190858af4828154119fd1344
richard Hu <huyongjinnuaa@163.com>
2020-02-03 14:51:45 +0800

    Richard producer consumer (#98)
    Richard producer consumer (#98)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,10 +1,18 @@
 package com.github.hcsp.multithread;
 
-public class ProducerConsumer1 {
-    public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+import java.util.Random;
 
+public class ProducerConsumer1 {
+    private static Integer random;
+
+    public static void main(String[] args) throws InterruptedException {
+        Object lock = new Object();
+
+        //分别雇佣了一个生产者和消费者工人
+        Producer producer = new Producer(lock);
+        Consumer consumer = new Consumer(lock);
+
+        //开始工作
         producer.start();
         consumer.start();
 
@@ -13,12 +21,56 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        Object lock;
+        //任何一个对象构造的时候需要先构造它的父亲
+        public Producer(Object lock) {
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+
+            for (int i = 0; i < 10; i++) {
+                //线程所有的工作都是在run里面进行的
+                synchronized (lock) {
+                    while (random != null) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    random = new Random().nextInt();
+                    System.out.println("Producing " + random);
+                    lock.notify();
+
+                }
+            }
+
+        }
     }
 
     public static class Consumer extends Thread {
+        Object lock;
+        public Consumer(Object lock) {
+            this.lock = lock;
+        }
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (random == null) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + random);
+                    random = null;
+                    lock.notify();
+                }
+            }
+        }
     }
 }
