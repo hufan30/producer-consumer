9276ed40f080117ef76942c6d709c21afbe0adc3
hufan30 <hufan30@163.com>
2020-08-19 14:59:05 +0800

    Update ProducerConsumer3.java

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -9,7 +9,6 @@ public class ProducerConsumer3 {
 
     public static void main(String[] args) throws InterruptedException {
 
-
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
 
